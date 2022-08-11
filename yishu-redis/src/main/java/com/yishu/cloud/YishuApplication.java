2c51f7f8ceba3b459fa5eae824d7a0625fd898da
yishu <2024160644@qq.com>
2022-08-11 16:42:30 +0800

    创建启动类

---
+++ yishu-redis/src/main/java/com/yishu/cloud/YishuApplication.java
@@ -0,0 +1,13 @@
+package com.yishu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YishuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YishuApplication.class,args);
+    }
+
+}
